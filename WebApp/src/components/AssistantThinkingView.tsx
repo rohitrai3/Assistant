ebe59c39cb7dfbb412effd96acc50b9f28d009fe
Rohit Rai <rohitraijob@gmail.com>
2026-04-10 16:48:28 +0530

    chore: Remove left border from assistant conversation

--- WebApp/src/components/AssistantThinkingView.tsx
+++ WebApp/src/components/AssistantThinkingView.tsx
@@ -9,7 +9,7 @@ export default function AssistantThinkingView({ content }: AssistantThinkingView
       <p className="text-xs opacity-25">
         Assistant: Thinking...
       </p>
-      <p className="text-sm text-gray border border-gray-dark font-gray rounded-r-2xl w-fit max-w-9/10 px-4 py-2">
+      <p className="text-sm text-gray border border-l-0 border-gray-dark font-gray rounded-r-2xl w-fit max-w-9/10 px-4 py-2">
         {content}
       </p>
     </div>
--- WebApp/src/components/AssistantToolView.tsx
+++ WebApp/src/components/AssistantToolView.tsx
@@ -10,7 +10,7 @@ export default function AssistantToolView({ name, input }: AssistantToolView) {
       <p className="text-xs opacity-25">
         Assistant: Tool...
       </p>
-      <p className="text-sm text-gray border border-gray-dark font-gray rounded-r-2xl w-fit max-w-9/10 px-4 py-2">
+      <p className="text-sm text-gray border border-l-0 border-gray-dark font-gray rounded-r-2xl w-fit max-w-9/10 px-4 py-2">
         Name: {name}, Input: {input}
       </p>
     </div>
